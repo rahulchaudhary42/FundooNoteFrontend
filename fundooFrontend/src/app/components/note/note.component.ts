c0af12784ccdc89cb3ebd0f0472d3f67b46dab8b
rahulchaudhary42 <rahulchaudhary7542@gmail.com>
2019-06-12 14:39:18 +0530

    add note

--- fundooFrontend/src/app/components/note/note.component.ts
+++ fundooFrontend/src/app/components/note/note.component.ts
@@ -11,41 +11,74 @@ import { MatSnackBar } from '@angular/material';
 export class NoteComponent implements OnInit {
   notes: any;
   unpinned: any[];
+  data1: any[];
   data: any[];
-   
-  constructor(private noteService: NoteService, private snackBar: MatSnackBar, private dataService: DataService) 
-  {
+  message: any;
+  pinned: any;
+
+  constructor(private noteService: NoteService, private snackBar: MatSnackBar, private dataService: DataService) {
 
   }
 
   ngOnInit() {
-    this.getNotes();
+    // this.getNotes();
+
+
+    this.dataService.currentMessage.subscribe(
+      (response: any) => {
+        this.message = response;
+        this.getUnPinned();
+        this.getPinned()
+        // this.getPinned() 
+
+      }
+    );
+
+
   }
 
-  //   pin(items: { id: string; }) 
-  pin(items) 
- 
-  {
-
+  //   for pin and unpin note
+  pin(items) {
     console.log(items.id);
-   
     this.noteService.putRequest("note/pin?id=" + items.id).subscribe(
       (response: any) => {
         if (response.statusCode === 1) {
           this.dataService.changeMessage(response.statusMessage);
-          this.snackBar.open(response.statusMessage,"close",{duration:2500});
+          this.snackBar.open(response.statusMessage, "close", { duration: 2500 });
         }
       }
     );
-    
   }
-  getNotes(){
-    this.noteService.getRequest("note/getallnotes").subscribe(
-      (response : any) => {
-       console.log('response from backend',response);
+
+  // for display all note of unpinned
+  getUnPinned() {
+    this.noteService.getRequest('note/getunpinnednotes').subscribe(
+      (response: any) => {
+        this.unpinned = response;
+        console.log(response);
+      }
+    )
+  }
+
+  // for display all note of pinned
+  getPinned() {
+    this.noteService.getRequest('note/getpinnednotes').subscribe(
+      (response: any) => {
+        // this.pinned = response;
         this.data = response;
-        console.log('array-->',this.data);
-        
+        console.log("pinned")
+        console.log(this.data);
+      }
+    )
+  }
+
+  // for display all note in dashboard
+  getNotes() {
+    this.noteService.getRequest("note/getallnotes").subscribe(
+      (response: any) => {
+        console.log('response from backend', response);
+        this.data1 = response;
+        console.log('array-->', this.data);
       }
     );
     console.log("Get notes");
